cea71faf5d0921004d2e568d734cd2c1905ed0f6
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-05-09 15:21:33 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <section className='container'>
+        <div className='apresentacao'>
+          <p>
+            Olá, sou <br/>
+            <span>Pedro Tramontini</span> <br/>
+            Dev Full Stack
+          </p>
+          <button className='btn btn-red'>
+            Saiba mais sobre mim 
+          </button>
+        </div>
+        <figure>
+          <img className='img-home' src='/developer-red.svg' alt="Imagem de Home"></img>
+        </figure>
+      </section>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            Feito com ❣️ por Pedro Tramontini &copy; 2025
+        </footer>
+    )
+}
+
+export default Footer
