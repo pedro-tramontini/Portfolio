e9308854ae458e8a74182bc99ce0ba6b553024c9
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-05-13 08:10:07 -0300

    feat: implementação dos componentes Cards, pegando dinamicamente as informações da api do github

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react"
 import Card from "../../components/Card"
+import styles from "./Projetos.module.css"
 
 function Projetos() {
-    return(
-        <>
-            <Card />
-            <Card />
-            <Card />
+    
+    const [repositories, setRepositories] = useState([])
 
-        </>
+    useEffect(() => {
+        const buscarRepositorios = async () => {
+            const response = await fetch('https://api.github.com/users/pedro-tramontini/repos')
+            const data = await response.json()
+            setRepositories(data)
+        }
+        buscarRepositorios()
+    }, [])
+    
+    return(
+        <section className={styles.projetos}>
+        {
+            repositories.length > 0 ? (
+                <section className={styles.projetos}>
+                    {
+                        repositories.map((repo) =>(
+                            <Card 
+                                key={repo.id} 
+                                name={repo.name} 
+                                description={repo.description} 
+                                html_url={repo.html_url}
+                            />
+                        ))
+                    }
+                </section>
+            ) : (
+                <p>Carregando respositórios...</p>
+            )
+        }
+        </section>
     )
 }
 
